c785a13359979f061caa078386e871376c07de15
pritha1993 <71940716+pritha1993@users.noreply.github.com>
2021-05-06 16:10:06 +0530

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,56 @@
+class Quiz {
+    constructor(){}
+  
+    getState(){
+      var gameStateRef  = database.ref('gameState');
+      gameStateRef.on("value",function(data){
+         gameState = data.val();
+      })
+  
+    }
+  
+    update(state){
+      database.ref('/').update({
+        gameState: state
+      });
+    }
+  
+    async start(){
+      if(gameState === 0){
+        contestant = new Contestant();
+        var contestantCountRef = await database.ref('contestantCount').once("value");
+        if(contestantCountRef.exists()){
+            contestantCount = contestantCountRef.val();
+            contestant.getCount();
+        }
+        question = new Question()
+        question.display();
+      }
+    }
+  
+    play(){
+      form.hide();
+      textSize(30);
+      text("Game Start", 120, 100)
+      Player.getPlayerInfo();
+  
+      if(allPlayers !== undefined){
+        var display_position = 130;
+        for(var plr in allPlayers){
+          if (plr === "player" + player.index)
+            fill("red")
+          else
+            fill("black");
+  
+          display_position+=20;
+          textSize(15);
+          text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        }
+      }
+  
+      if(keyIsDown(UP_ARROW) && player.index !== null){
+        player.distance +=50
+        player.update();
+      }
+    }
+  }
